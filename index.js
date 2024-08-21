91c9788c44781a8cd57f6560cbaefc9cecd7c27c
Nixxa <yerson.onepiece@gmail.com>
2024-08-20 19:49:47 -0500

    Se hizo todas las consultas necesarioa a la api

--- index.js
+++ index.js
@@ -7,14 +7,37 @@ function getRandomIntInclusive(min, max) {
 const text_name = document.getElementById("name");
 const text_description = document.getElementById("description");
 const text_type = document.getElementById("type");
+const imgElement = document.getElementById("img");
 
 let randomNum = getRandomIntInclusive(1, 1025);
 
-fetch("https://pokeapi.co/api/v2/pokemon/1/")
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}/`)
   .then((response) => response.json())
   .then((data) => {
     text_name.innerText = data.name;
-    text_description.innerText = data.name;
-    text_type.innerText = data.name;
+    imgElement.src = data.sprites.front_default;
+    imgElement.alt = data.name;
+
+    // Obtener los nombres de los tipos
+    let typePromises = data.types.map((type) =>
+      fetch(type.type.url)
+        .then((response) => response.json())
+        .then((typeData) => {
+          return typeData.names.find((entry) => entry.language.name === "es")
+            .name;
+        })
+    );
+
+    // Esperar a que todas las promesas de tipos se resuelvan
+    return Promise.all(typePromises).then((typeNames) => {
+      text_type.innerText = typeNames.join(", ");
+      return fetch(data.species.url);
+    });
+  })
+  .then((response) => response.json())
+  .then((speciesData) => {
+    text_description.innerText = speciesData.flavor_text_entries.find(
+      (entry) => entry.language.name === "es"
+    ).flavor_text;
   })
   .catch((error) => console.error("Error:", error));
