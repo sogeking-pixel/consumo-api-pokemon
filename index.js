09d98b01d5c30cc43af3ea38c6558d45f510fb7d
Nixxa <yerson.onepiece@gmail.com>
2024-08-20 19:18:22 -0500

    se inserto dato de la api al html

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const text_name = document.getElementById("name");
+const text_description = document.getElementById("description");
+const text_type = document.getElementById("type");
+
 let randomNum = getRandomIntInclusive(1, 1025);
 
 fetch("https://pokeapi.co/api/v2/pokemon/1/")
   .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    text_name.innerText = data.name;
+    text_description.innerText = data.name;
+    text_type.innerText = data.name;
+  })
   .catch((error) => console.error("Error:", error));
-
